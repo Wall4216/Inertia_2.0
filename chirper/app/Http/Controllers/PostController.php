2b9a059583bbeaa1beb0de5934ae2a31b3ede5b2
Wall <ilnazamirhanov61@gmail.com>
2023-01-14 02:08:59 +0300

    add correction backend part

---
+++ chirper/app/Http/Controllers/PostController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Post\StoreRequest;
+use App\Http\Resources\Post\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::all();
+
+        $posts = PostResource::collection($posts)->resolve();
+        return inertia('Post/Index', compact('posts'));
+    }
+    public function create()
+    {
+        return inertia('Post/Create');
+    }
+    public function store(StoreRequest $request)
+    {
+        Post::create($request->validated());
+        return redirect()->route('post.index');
+    }
+}
